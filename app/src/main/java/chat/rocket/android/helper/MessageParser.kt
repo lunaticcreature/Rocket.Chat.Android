319619b868bc44fcdb8291fa977dba060beb0716
bizzbyster <bizzbyster@gmail.com>
2018-03-29 22:46:21 -0500

    updates

--- app/src/main/java/chat/rocket/android/helper/MessageParser.kt
+++ app/src/main/java/chat/rocket/android/helper/MessageParser.kt
@@ -172,7 +172,7 @@ class MessageParser @Inject constructor(
                                 if (link.startsWith("http://www.requiresinternet.com")) {
                                     val layout = LayoutInflater.from(context).inflate(R.layout.custom_toast, findViewById(R.id.custom_toast_container))
                                     val text = layout.findViewById<TextView>(R.id.text)
-                                    text.setText("Please credit your Viasat account to access this page")
+                                    text.setText("Can I help you get access to this content?")
                                     val toast =  Toast(context)
                                     toast.setGravity(Gravity.FILL_HORIZONTAL or Gravity.BOTTOM, 0, 0)
                                     toast.setDuration(Toast.LENGTH_LONG)
